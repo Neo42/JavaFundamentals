b11b48d06065b3b791e59bdd1cbb1a5d6792b23e
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2023-09-21 19:55:45 +0930

    feat: ✨ add code for chapter 6

---
+++ src/OOP/Chapter6/FoxAndRabbit/cell/Cell.java
@@ -0,0 +1,7 @@
+package OOP.Chapter6.FoxAndRabbit.cell;
+
+import java.awt.Graphics;
+
+public interface Cell {
+	void draw(Graphics g, int x, int y, int size);
+}
---
+++ src/OOP/Chapter6/Shapes/Shape.java
@@ -0,0 +1,9 @@
+package OOP.Chapter6.Shapes;
+
+import java.awt.Graphics;
+
+public abstract class Shape {
+
+    public abstract void draw(Graphics g);
+
+}
